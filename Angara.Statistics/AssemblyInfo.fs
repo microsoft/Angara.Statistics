d8fd665492bf32068ebe3a5111decb90dbffd756
Dmitry Voytsekhovskiy <v-dmvo@microsoft.com>
2015-12-09 17:05:16 +0300

    Adding Angara.Statistics project

---
+++ Angara.Statistics/AssemblyInfo.fs
@@ -0,0 +1,38 @@
+﻿namespace Angara.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+[<assembly: AssemblyTitle("Angara.Statistics")>]
+[<assembly: AssemblyDescription("A random number generator, probability distributions and gaussian kernel density estimator.")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("Microsoft Research")>]
+[<assembly: AssemblyProduct("Angara.Statistics")>]
+[<assembly: AssemblyCopyright("Copyright ©  2015")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("7dc6271f-642e-4431-bb7c-24f49a36413c")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("0.1.0.0")>]
+[<assembly: AssemblyFileVersion("0.1.0.0")>]
+
+do
+    ()
