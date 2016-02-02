0f61728451c4eeeeb4608a6c6f93570c6d94a5eb
Vassily Lyutsarev <vassilyl@microsoft.com>
2016-02-02 00:52:16 +0000

    Filzbach Parameters

---
+++ Angara.Statistics.Tests/FilzbachTests.fs
@@ -0,0 +1,122 @@
+﻿module FilzbachTests
+open NUnit.Framework
+open FsUnit
+
+open Angara.Filzbach
+
+[<Test>]
+let ParametersTests() =
+    let p0 = Parameters()
+    p0.CountValues |> should equal 0
+    p0.AllValues |> should equal [||]
+    (fun () -> p0.GetValue 0 |> ignore) |> should throw typeof<System.IndexOutOfRangeException>
+    (fun () -> p0.GetValue "unknown" |> ignore) |> should throw typeof<System.Collections.Generic.KeyNotFoundException>
+    (fun () -> p0.GetValue("unknown",0) |> ignore) |> should throw typeof<System.Collections.Generic.KeyNotFoundException>
+    (fun () -> p0.GetDefinition "unknown" |> ignore) |> should throw typeof<System.Collections.Generic.KeyNotFoundException>
+    (p0:>IParameters).Count |> should equal 0
+
+    // scalar
+    (fun () -> p0.Add(null, [|0.5|],0.,1.) |> ignore) |> should throw typeof<System.ArgumentException> // empty name
+    (fun () -> p0.Add("", [|0.5|],0.,1.) |> ignore) |> should throw typeof<System.ArgumentException> // empty name
+    (fun () -> p0.Add("s", [||],0.,1.) |> ignore) |> should throw typeof<System.ArgumentException> // empty values
+    (fun () -> p0.Add("s", [|0.5|],1.,0.) |> ignore) |> should throw typeof<System.ArgumentException> // lower>upper
+    (fun () -> p0.Add("s", [|0.|],0.5,1.) |> ignore) |> should throw typeof<System.ArgumentException> // value out of range
+    (fun () -> p0.Add("s", [|0.5|],0.,1.,isLog=true) |> ignore) |> should throw typeof<System.ArgumentException> // lower for log par
+    let p1 = p0.Add("s", [|0.5|],0.,1.)
+    p0.Add("s", [|0.5|],0.,1.) |> should equal p1
+    (fun () -> p1.Add("s", [|0.5|],0.,1.) |> ignore) |> should throw typeof<System.ArgumentException> // duplicate name
+    p1.CountValues |> should equal 1
+    p1.AllValues |> List.ofSeq |> should equal [0.5]
+    p1.GetValue 0 |> should equal 0.5
+    (fun () -> p1.GetValue 1 |> ignore) |> should throw typeof<System.IndexOutOfRangeException>
+    (fun () -> p1.GetValue -1 |> ignore) |> should throw typeof<System.IndexOutOfRangeException>
+    p1.GetValue "s" |> should equal 0.5
+    p1.GetValue("s", 0) |> should equal 0.5
+    (fun () -> p1.GetValue("s", 1) |> ignore) |> should throw typeof<System.IndexOutOfRangeException>
+    p1.GetDefinition "s" |> should equal {index=0; size=1; lower=0.; upper=1.; isFixed=false; isLog=false}
+    (p1:>IParameters).Count |> should equal 1
+    (p1:>IParameters).ContainsKey "s" |> should be True
+    (p1:>IParameters).ContainsKey "a" |> should be False
+    (p1:>IParameters).["s"] |> should equal [|0.5|]
+    let v = ref [||] 
+    (p1:>IParameters).TryGetValue("s", v) |> should be True
+    !v |> should equal [|0.5|]
+    (p1:>IParameters).Keys |> Seq.toList |> should equal ["s"]
+    (p1:>IParameters).Values |> Seq.toList |> should equal [[|0.5|]]
+
+    // vector
+    (fun () -> p0.Add("v", [|0.5; -0.5|],0.,1.) |> ignore) |> should throw typeof<System.ArgumentException> // value out of range
+    let p2 = p0.Add("v", [|0.6;0.7|],0.,1.)
+    p2.CountValues |> should equal 2
+    p2.AllValues |> List.ofSeq |> should equal [0.6;0.7]
+    p2.GetValue 0 |> should equal 0.6
+    p2.GetValue 1 |> should equal 0.7
+    (fun () -> p2.GetValue "v" |> ignore) |> should throw typeof<System.InvalidOperationException> // vector syntax
+    p2.GetValue("v", 0) |> should equal 0.6
+    p2.GetValue("v", 1) |> should equal 0.7
+    p2.GetDefinition "v" |> should equal {index=0; size=2; lower=0.; upper=1.; isFixed=false; isLog=false}
+    (p2:>IParameters).Count |> should equal 1
+    (p2:>IParameters).ContainsKey "v" |> should be True
+    (p2:>IParameters).ContainsKey "a" |> should be False
+    (p2:>IParameters).["v"] |> should equal [|0.6;0.7|]
+    (p2:>IParameters).TryGetValue("v", v) |> should be True
+    !v |> should equal [|0.6;0.7|]
+    (p2:>IParameters).Keys |> Seq.toList |> should equal ["v"]
+    (p2:>IParameters).Values |> Seq.toList |> should equal [[|0.6;0.7|]]
+
+    // second scalar
+    let p3 = p2.Add("s", [|0.5|],0.,1.)
+    p2.Add("s", [|0.5|],0.,1.) |> should equal p3
+    (fun () -> p1.Add("s", [|0.5|],0.,1.) |> ignore) |> should throw typeof<System.ArgumentException> // duplicate name
+    p3.CountValues |> should equal 3
+    p3.AllValues |> List.ofSeq |> should equal [0.6;0.7;0.5]
+    p3.GetValue 0 |> should equal 0.6
+    p3.GetValue 1 |> should equal 0.7
+    p3.GetValue 2 |> should equal 0.5
+    p3.GetValue("v", 0) |> should equal 0.6
+    p3.GetValue("v", 1) |> should equal 0.7
+    p3.GetValue "s" |> should equal 0.5
+    p3.GetValue("s", 0) |> should equal 0.5
+    p3.GetDefinition "s" |> should equal {index=2; size=1; lower=0.; upper=1.; isFixed=false; isLog=false}
+    p3.GetDefinition "v" |> should equal {index=0; size=2; lower=0.; upper=1.; isFixed=false; isLog=false}
+    (p3:>IParameters).Count |> should equal 2
+    (p3:>IParameters).ContainsKey "s" |> should be True
+    (p3:>IParameters).ContainsKey "v" |> should be True
+    (p3:>IParameters).ContainsKey "a" |> should be False
+    (p3:>IParameters).["s"] |> should equal [|0.5|]
+    (p3:>IParameters).["v"] |> should equal [|0.6;0.7|]
+    (p3:>IParameters).TryGetValue("s", v) |> should be True
+    !v |> should equal [|0.5|]
+    (p3:>IParameters).TryGetValue("v", v) |> should be True
+    !v |> should equal [|0.6;0.7|]
+    (p3:>IParameters).Keys |> Seq.toList |> should equal ["s";"v"]
+    (p3:>IParameters).Values |> Seq.toList |> should equal [[|0.5|]; [|0.6;0.7|]]
+
+    // second vector
+    let p4 = p1.Add("v", [|0.6;0.7|],0.,1.)
+    p1.Add("v", [|0.6;0.7|],0.,1.) |> should equal p4
+    p4.CountValues |> should equal 3
+    p4.AllValues |> List.ofSeq |> should equal [0.5;0.6;0.7]
+    p4.GetValue 0 |> should equal 0.5
+    p4.GetValue 1 |> should equal 0.6
+    p4.GetValue 2 |> should equal 0.7
+    p4.GetValue("v", 0) |> should equal 0.6
+    p4.GetValue("v", 1) |> should equal 0.7
+    p4.GetValue "s" |> should equal 0.5
+    p4.GetValue("s", 0) |> should equal 0.5
+    p4.GetDefinition "s" |> should equal {index=0; size=1; lower=0.; upper=1.; isFixed=false; isLog=false}
+    p4.GetDefinition "v" |> should equal {index=1; size=2; lower=0.; upper=1.; isFixed=false; isLog=false}
+    (p4:>IParameters).Count |> should equal 2
+    (p4:>IParameters).ContainsKey "s" |> should be True
+    (p4:>IParameters).ContainsKey "v" |> should be True
+    (p4:>IParameters).ContainsKey "a" |> should be False
+    (p4:>IParameters).["s"] |> should equal [|0.5|]
+    (p4:>IParameters).["v"] |> should equal [|0.6;0.7|]
+    (p4:>IParameters).TryGetValue("s", v) |> should be True
+    !v |> should equal [|0.5|]
+    (p4:>IParameters).TryGetValue("v", v) |> should be True
+    !v |> should equal [|0.6;0.7|]
+    (p4:>IParameters).Keys |> Seq.toList |> should equal ["s";"v"]
+    (p4:>IParameters).Values |> Seq.toList |> should equal [[|0.5|]; [|0.6;0.7|]]
+
+
---
+++ Angara.Statistics/Filzbach.fs
@@ -0,0 +1,93 @@
+﻿module Angara.Filzbach
+
+type ParameterDefinition = {
+    index: int
+    size: int
+    lower: float
+    upper: float
+    isLog: bool
+    isFixed: bool
+    }
+
+type IParameters = System.Collections.Generic.IReadOnlyDictionary<string, float[]>
+
+type Parameters private (pdefs: Map<string,ParameterDefinition>, pvalues: float[]) =
+    let defaultLog isLog = defaultArg isLog false 
+    let defaultFixed isFixed = defaultArg isFixed false
+    let avalue pdef = Array.sub pvalues pdef.index pdef.size
+    let all = pdefs |> Seq.map (fun kv -> 
+        System.Collections.Generic.KeyValuePair<string, float[]>(kv.Key, avalue kv.Value))
+    do
+        assert(Array.length pvalues = Map.fold (fun sum _ def -> sum + def.size) 0 pdefs)
+    new() = Parameters(Map.empty,[||])
+    member private x.definitions = pdefs
+    member private x.values = pvalues
+    member x.Add(name, values, lower, upper, ?isLog, ?isFixed) =
+        if name = null || name = "" || Map.containsKey name pdefs then 
+            invalidArg "name" "parameters must have unique non-empty names."
+        if Array.length values < 1 then
+            invalidArg "values" "empty values array, each parameter must have at least one value."
+        if lower > upper then
+            invalidArg "lower" "lower must not be greater than upper."
+        values |> Array.iteri (fun i v ->
+            if v < lower || v > upper then
+                invalidArg "values" (sprintf "values[%d] is out of [lower..upper] range" i))
+        if isLog.IsSome && isLog.Value && lower <= 0. then 
+            invalidArg "lower" "lower must not be greater than upper."
+        Parameters(
+            pdefs |> Map.add name {
+                index = Array.length pvalues 
+                size = Array.length values
+                lower = lower
+                upper = upper
+                isLog = defaultLog isLog
+                isFixed = defaultFixed isFixed || lower=upper
+                }, 
+            Array.append pvalues values)
+    member x.Add(name, value, lower, upper, ?isLog, ?isFixed) =
+        // by default, the parameter is a scalar value
+        x.Add(name, [|value|], lower, upper, defaultLog isLog, defaultFixed isFixed)
+    member x.Add(name, size:int, value:float, lower:float, upper:float, ?isLog, ?isFixed) = 
+        if size<1 then
+            invalidArg "size" "parameter size must be 1 or greater."
+        x.Add(name, Array.create size value, lower, upper, defaultLog isLog, defaultFixed isFixed)
+    member x.SetValues(values) =
+        if Array.length values <> Array.length pvalues then
+            invalidArg "values" "invalid length of the array."
+        Parameters(pdefs, values)
+    member x.GetValue idx = pvalues.[idx]
+    member x.GetValue name =
+        let d = pdefs.[name]
+        if d.size>1 then invalidOp ("use getValue(name,index) for the vector parameter "+name+".")
+        pvalues.[d.index]
+    member x.GetValue(name,idx) = 
+        let d = pdefs.[name]
+        if idx<0 || idx >= d.size then raise(System.IndexOutOfRangeException())
+        pvalues.[d.index + idx]
+    member x.GetValues name = avalue pdefs.[name]
+    member x.AllValues = Seq.ofArray pvalues
+    member x.CountValues = Array.length pvalues
+    member x.GetDefinition name = pdefs.[name]
+    interface IParameters with
+        member x.Count = pdefs.Count
+        member x.ContainsKey key = pdefs.ContainsKey key
+        member x.Keys = pdefs |> Seq.map (fun kv -> kv.Key)
+        member x.Values = all |> Seq.map (fun kv -> kv.Value)
+        member x.get_Item key = avalue pdefs.[key]
+        member x.TryGetValue(key, value: byref<float[]>) =
+            if pdefs.ContainsKey key then
+                value <- avalue pdefs.[key]
+                true
+            else false
+    interface System.Collections.Generic.IEnumerable<System.Collections.Generic.KeyValuePair<string,float[]>> with
+        member x.GetEnumerator() = all.GetEnumerator()
+    interface System.Collections.IEnumerable with
+        member x.GetEnumerator() = (all:>System.Collections.IEnumerable).GetEnumerator()
+    override x.Equals other =
+        match other with
+        | :? Parameters as y -> y.definitions = x.definitions && y.values = x.values
+        | _ -> false
+    override x.GetHashCode() = pdefs.GetHashCode() ^^^ pvalues.GetHashCode()
+
+
+
