07c126fab49e0ff1917d2f6b19037c6495cae9b1
Vassily Lyutsarev <vassilyl@microsoft.com>
2016-02-05 13:20:58 +0000

    Filzbach sampler ready to start checking

--- Angara.Statistics/Filzbach.fs
+++ Angara.Statistics/Filzbach.fs
@@ -18,6 +18,7 @@ type ParameterDefinition =
 
 type IParameters = System.Collections.Generic.IReadOnlyDictionary<string, float[]>
 
+/// A container for model parameters.
 type Parameters private (pdefs: Map<string,ParameterDefinition>, pvalues: float[]) =
     let defaultLog isLog = defaultArg isLog false 
     let defaultDelay delay = defaultArg delay 0
@@ -31,7 +32,19 @@ type Parameters private (pdefs: Map<string,ParameterDefinition>, pvalues: float[
     new() = Parameters(Map.empty,[||])
     member internal x.definitions = pdefs
     member internal x.values = pvalues
-    member x.Add(name, values, lower, upper, ?delay, ?isLog, ?prior) =
+
+    /// <summary>Adds a parameter to the container.</summary>
+    /// <param name="name">Parameter name.</param>
+    /// <param name="values">An array of one or more parameter values.</param>
+    /// <param name="lower">Lower bound of parameter values.</param>
+    /// <param name="upper">Upper bound of parameter values.</param>
+    /// <param name="delay">Sampler behaviour: &lt;1 (default) -- initialize values with random numbers;
+    /// =1 -- start with the values supplied in the call;
+    /// &gt;1 -- release the parameter values after 'delay' iterations.</param>
+    /// <param name="isLog">If true, the sampler will use logarithmic transform for the parameter.
+    /// The default is false.</param>
+    /// <param name="prior">Prior knowledge. The default is a non-informative prior.</param>
+    member x.Add(name, values,  lower, upper, ?delay, ?isLog, ?prior) =
         if name = null || name = "" || Map.containsKey name pdefs then 
             invalidArg "name" "parameters must have unique non-empty names."
         if Array.length values < 1 then
@@ -68,23 +81,49 @@ type Parameters private (pdefs: Map<string,ParameterDefinition>, pvalues: float[
         if size<1 then
             invalidArg "size" "parameter size must be 1 or greater."
         x.Add(name, Array.create size value, lower, upper, delay, defaultLog isLog, defaultPrior prior)
-    member x.SetValues(values) =
+
+    /// Replaces all parameter values
+    member x.SetValues
+            /// An array of all parameters values.
+            /// For a parameter 'name' the parameter values start with index '.GetDefinition(name).index' in the array.
+            values =
         if Array.length values <> Array.length pvalues then
             invalidArg "values" "invalid length of the array."
         Parameters(pdefs, values)
+
+    /// Fast access to parameter values. See <see cref="SetValues"/> for explanation of indices.
     member x.GetValue idx = pvalues.[idx]
+
+    /// Get a value of a scalar parameter.
     member x.GetValue name =
         let d = pdefs.[name]
         if d.size>1 then invalidOp ("use getValue(name,index) for the vector parameter "+name+".")
         pvalues.[d.index]
+
+    /// Get a value of a vector parameter.
     member x.GetValue(name,idx) = 
         let d = pdefs.[name]
         if idx<0 || idx >= d.size then raise(System.IndexOutOfRangeException())
         pvalues.[d.index + idx]
+
+    /// Get an array of all values of a parameter.
     member x.GetValues name = avalue pdefs.[name]
+
+    /// Get all values of all parameters.
+    /// For a parameter 'name' the parameter values start with index '.GetDefinition(name).index' in the sequence.
     member x.AllValues = Seq.ofArray pvalues
+
+    /// Total number of all parameter values.
     member x.CountValues = Array.length pvalues
+
+    /// Get a parameter definition.
     member x.GetDefinition name = pdefs.[name]
+
+    /// Get a parameter name by value index.
+    member x.GetName idx =
+        if idx < 0 || idx >= Array.length pvalues then raise(System.IndexOutOfRangeException())
+        Map.findKey (fun n d -> idx >= d.index && idx < d.index+d.size) pdefs
+
     interface IParameters with
         member x.Count = pdefs.Count
         member x.ContainsKey key = pdefs.ContainsKey key
@@ -106,43 +145,36 @@ type Parameters private (pdefs: Map<string,ParameterDefinition>, pvalues: float[
         | _ -> false
     override x.GetHashCode() = pdefs.GetHashCode() ^^^ pvalues.GetHashCode()
 
-//type internal psampler = {
-//    value: float
-//    prior: Distribution option
-//    ival: float
-//    decis: int
-//    delta: float
-//    pold: float
-//    padd: float
-//    altt: int
-//    runalt: int
-//    runacc: int
-//    }
-type Sampler private (pp:Parameters,
-                      logl: Parameters -> float,
+
+/// An immutable state of Filzbach MCMC sampler.
+type Sampler private (logl: Parameters -> float,
                       // utilities
                       pall: ParameterDefinition[],
                       // variables
                       metr_k: int,
                       rng: MT19937,
-                      values: float[],
+                      pp:Parameters,
                       deltas:float[],
                       ltotold: float,
                       ptotold:float,
-                      pbcount: int, // probe count
-                      pbacc: int, // accepted probes
+                      accept: bool, // the probe has been accepted
                       runalt:int[], // number of alterations of individual parameters
                       runacc:int[] // number of accepted alterations of individual parameters
     ) =
-    let log_likelihood values = pp.SetValues values |> logl
-    let log_prior values = Array.fold2 (fun sum d v -> 
+    static let log_prior pall values = Array.fold2 (fun sum d v -> 
             match d.prior with None -> sum | Some p -> sum + log_pdf p v) 0. pall values
-    member x.Create(pp: Parameters, rng: MT19937, logl: Parameters -> float) =
+    static let make_pall (pp:Parameters) =
+        let pall = Array.zeroCreate pp.CountValues
+        for kv in pp.definitions do
+            let pdef = kv.Value
+            let index = pdef.index
+            for offset in 0..pdef.size-1 do
+                pall.[index+offset] <- pdef
+        pall
+
+    static member Create(pp: Parameters, rng: MT19937, logl: Parameters -> float) =
         // init_chains
-        let pall =
-            pp.definitions 
-            |> Seq.map (fun kv -> let d = kv.Value in Array.create d.size d)
-            |> Array.concat
+        let pall = make_pall pp
         let paramcount = pall.Length
         // initRandomValues
         let values = pall |> Array.mapi (fun i def ->
@@ -156,25 +188,6 @@ type Sampler private (pp:Parameters,
             else 0.50*(def.upper - def.lower))
         let runalt = Array.create paramcount 0
         let runacc = Array.create paramcount 0
-//        let pss = 
-//            pp.definitions 
-//            |> Seq.map (fun kv ->
-//                let def = kv.Value
-//                Array.init def.size (fun i ->
-//                    // initRandomValues
-//                    let v = (if def.delay<1 
-//                            then Uniform(def.lower, def.upper) |> draw rng
-//                            else pp.values.[def.index + i]);
-//                    {ival=0.; decis=0; prior=def.prior;
-//                    // initStepSizes 
-//                    delta = (if def.isLog
-//                            then 0.50*(log def.upper - log def.lower)
-//                            else 0.50*(def.upper - def.lower));
-//                    value = v;
-//                    // init_paramerniformation
-//                    pold = v; 
-//                    padd=0.; altt=0; runalt=0; runacc=0}))
-//            |> Array.concat
         // init_counters
         let metr_number_ok = 0
         let pbacc = 0
@@ -182,15 +195,16 @@ type Sampler private (pp:Parameters,
         // init_bayestable
         let bayestable = []
         // init_likelihood
-        let ltotold = log_likelihood values
-        let ptotold =  log_prior values
+        let ltotold = pp.SetValues values |> logl
+        let ptotold =  log_prior pall values
         // initialize iteration number
         let metr_k = 1
-        Sampler(pp, logl, pall, metr_k, rng, values, deltas, ltotold, ptotold, 0, 0, runalt, runacc)
-    member x.BurinIn() = 
+        Sampler(logl, pall, metr_k, rng, pp.SetValues values, deltas, ltotold, ptotold, false, runalt, runacc)
+
+    member x.Probe(isBurnIn:bool) =
         let paramcount = pall.Length
         let rng = MT19937(rng)
-        let newvalues = Array.copy values
+        let values = Array.copy pp.values
         let deltas = Array.copy deltas
         let runalt = Array.copy runalt
         let runacc = Array.copy runacc
@@ -198,7 +212,9 @@ type Sampler private (pp:Parameters,
         // Select parameters to change. Most of the time change only one, two or three params.
         let alterable = [for i in 0..paramcount-1 do if pall.[i].delay < metr_k then yield i]
         let freeparamcount = alterable.Length
-        //TODO: if freeparamcount=0 then {this with metr_k = metr_k + 1} else
+        if freeparamcount=0 then 
+            Sampler(logl, pall, metr_k+1, rng, pp, deltas, ltotold, ptotold, accept, runalt, runacc)
+        else
         let alt = // chain_params[i].alt=1 ~ alt |> List.any (fun item -> item=i)
             if freeparamcount=1 then alterable // one parameter always alters
             elif rng.uniform_float64() < 0.670 then
@@ -228,18 +244,18 @@ type Sampler private (pp:Parameters,
 
         // change parameter values, i.e. make the 'jump'
         for i in alt do
-            let old = newvalues.[i]
-            newvalues.[i] <- infinity
-            while newvalues.[i]<pall.[i].lower || newvalues.[i]>pall.[i].upper do
+            let old = values.[i]
+            values.[i] <- infinity
+            while values.[i]<pall.[i].lower || values.[i]>pall.[i].upper do
                 let add = rng.normal() * deltas.[i]
                 if pall.[i].isLog then
-                    newvalues.[i] <- old * exp(add)
+                    values.[i] <- old * exp(add)
                 else
-                    newvalues.[i] <- old + add
+                    values.[i] <- old + add
 
         // calc new lnlike
-        let ltotnew = log_likelihood newvalues
-        let ptotnew = log_prior newvalues
+        let ltotnew = pp.SetValues values |> logl
+        let ptotnew = log_prior pall values
 
         // compare new to old and accept or reject -- METROPOLIS CRITERION IS IN HERE
         let accept = 
@@ -249,102 +265,74 @@ type Sampler private (pp:Parameters,
             log rndnum < dlik)
 
         // act on acceptance
-        let bcount = pbcount + 1
-        let pbacc = if accept then pbacc + 1 else pbacc
         for i in alt do
             runalt.[i] <- runalt.[i]+1
             if accept then runacc.[i] <- runacc.[i]+1
 
-        // adjust jump steps? only if in burnin
-        for ii in 0..paramcount-1 do
-            if runalt.[ii]=20 then
-                if runacc.[ii] < 5 then
-                    // decrease temperature by 20%
-                    deltas.[ii] <- deltas.[ii] * 0.80
-                elif runacc.[ii] > 5 then
-                    // increase temperature by 20%
-                    deltas.[ii] <- deltas.[ii] * 1.20
-                runalt.[ii] <- 0
-                runacc.[ii] <- 0
-                //if pall.[ii].isLog then
+        if isBurnIn then
+            // adjust jump steps
+            for ii in 0..paramcount-1 do
+                if runalt.[ii]=20 then 
+                    let dmin, dmax = 
+                        let def = pall.[ii]
+                        if def.isLog then 0.010, 10.0 //TODO: ????
+                        else let full = def.upper-def.lower in 0.0010*full, 0.50*full
+                    if runacc.[ii] < 5 then
+                        // decrease temperature by 20%
+                        deltas.[ii] <- max dmin (min dmax (deltas.[ii] * 0.80))
+                    elif runacc.[ii] > 5 then
+                        // increase temperature by 20%
+                        deltas.[ii] <- max dmin (min dmax (deltas.[ii] * 1.20))
+                    runalt.[ii] <- 0
+                    runacc.[ii] <- 0
 
         if (accept) then
-            Sampler(pp, logl, pall, metr_k, rng, newvalues, deltas, ltotnew, ptotnew, pbcount, pbacc, runalt, runacc)
+            Sampler(logl, pall, metr_k+1, rng, pp.SetValues values, deltas, ltotnew, ptotnew, accept, runalt, runacc)
         else
-            Sampler(pp, logl, pall, metr_k, rng, values, deltas, ltotold, ptotold, pbcount, pbacc, runalt, runacc)
-    (*
+            Sampler(logl, pall, metr_k+1, rng, pp, deltas, ltotold, ptotold, accept, runalt, runacc)
 
-			/* adjust jump steps? only if in burnin */
-			if (metr_k < burnin)
-			{
-				for (int ii = 0; ii < paramcount; ii++)
-				{
-					if (chain_params[ii].runalt == 20 && chain_params[ii].fixed < 1)
-					{
-						chain_params[ii].runalt = 0;
+    member x.Parameters = pp
+    member x.LogLikelihood = ltotold
+    member x.LogPrior = ptotold
+    member x.SamplingWidths = Array.copy deltas
+    member x.IsAccepted = accept
 
-						if (chain_params[ii].runacc < 5)
-						{
-							/* decrease temperature */
-							chain_params[ii].delta *= 0.80;
-						}
-						if (chain_params[ii].runacc > 5)
-						{
-							/* increase temperature */
-							chain_params[ii].delta *= 1.20;
-						}
-						chain_params[ii].runacc = 0;
+    static member runmcmc(pp, logl, burnCount, sampleCount, ?thinning, ?rng) =
+        let thinning = defaultArg thinning 100
+        if thinning<1 then invalidArg "thinning" "must be > 0."
+        let rng = defaultArg rng (MT19937())
+        // initialize sampler
+        let mutable sampler = Sampler.Create(pp, rng, logl)
+        // do burn-in iterations
+        for _ in 1..burnCount do sampler <- sampler.Probe(true)
+        // collect sampleCount samples
+        let mutable countAccepted = 0
+        let samples = 
+            [
+            for _ in 1..sampleCount ->
+                for _ in 1..thinning do
+                    sampler <- sampler.Probe(false)
+                    if sampler.IsAccepted then countAccepted <- countAccepted + 1
+                Array.copy sampler.Parameters.values, sampler.LogLikelihood, sampler.LogPrior
+            ]
+        sampler, samples, ((float countAccepted) / (float sampleCount * float thinning))
 
-						if (chain_params[ii].type > 0) {
-							if (chain_params[ii].delta < 0.010)
-								chain_params[ii].delta = 0.010;
-							if (chain_params[ii].delta > 10.0)
-								chain_params[ii].delta = 10.0;
-						}
-						else {
-							if (chain_params[ii].delta < (0.0010*(chain_params[ii].ub - chain_params[ii].lb)))
-								chain_params[ii].delta = (0.0010*(chain_params[ii].ub - chain_params[ii].lb));
-							if (chain_params[ii].delta > (0.50*(chain_params[ii].ub - chain_params[ii].lb)))
-								chain_params[ii].delta = (0.50*(chain_params[ii].ub - chain_params[ii].lb));
-						}
-					} // if runalt == 20
-				} // ii loop
-			} // if still in first burnin
-
-			/* keeping track of decisions thing */
-			for (int ii = 0; ii < paramcount; ii++)
-				if (chain_params[ii].alt < 1)
-					chain_params[ii].decis = 0;
-
-			if (metr_k % bayes_step == 0 && metr_k >= tburnin  && metr_k <= (tburnin + teststeps) && accept_flag)
-			{
-				// write sample into memory
-				fill_bayestable(chain, metr_k);
-				accept_flag = false;
-			} // if outputting sample
-
-			/* -------------- write to another file that logs everything (this file not used any more) ------------- */
-			if (accept && out_chain)
-			{
-#pragma omp critical
-			{
-				mfile = workspace_fopen(out_chain_name, "a");
-				// output chain, iteration
-				fprintf(mfile, "%d\t%d\t%lf\t%lf", chain, metr_k, ltotnew[chain] + ptotnew[chain], ltotnew[chain]);
-				// parameter values
-				param* p = current_para();
-				for (int i = 0; i < paramcount; i++)
-				{
-					fprintf(mfile, "\t%lf", p[i].value);
-				}
-				fprintf(mfile, "\n");
-				fclose(mfile);
-			}
-
-			}
-
-			/* unforce any forced accepts, rejects */
-			accept_forced[currentchain] = 0;
-			reject_forced[currentchain] = 0;
-    *)
-    member x.Sample() = ()
+    static member print(sampler:Sampler, samples:(float[]*float*float) seq, acceptanceRate) =
+        printfn "Samples max log likelihood*prior = %g, acceptance rate at sampling = %5.3f" 
+                    (samples |> Seq.map (fun (_,logl,logp) -> logl+logp) |> Seq.max)
+                    acceptanceRate
+        printfn "------------+------------+------------+------------+------------+------------+------------+------------+"
+        printfn "       name |      lower |  lower 95%% |  lower 68%% |     median |  upper 68%% |  upper 95%% |      upper | prior"
+        printfn "------------+------------+------------+------------+------------+------------+------------+------------+"
+        for idx in 0..sampler.Parameters.CountValues-1 do
+            let q = qsummary (samples |> Seq.map (fun (sample,_,_) -> sample.[idx]))
+            let name = sampler.Parameters.GetName idx
+            let pdef = sampler.Parameters.GetDefinition name
+            let fullname = if pdef.size=1 then name else sprintf "%s[%d]" name (idx-pdef.index)
+            printfn " %10s | %10g | %10g | %10g | %10g | %10g | %10g | %10g | %A"
+                    (fullname.Substring(0,min 10 fullname.Length))
+                    pdef.lower
+                    q.lb95 q.lb68 q.median q.ub68 q.ub95
+                    pdef.upper pdef.prior
+            for off in 10..10..fullname.Length do
+            printfn " %10s" (fullname.Substring(off,min 10 (fullname.Length-off)))
